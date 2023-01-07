f805887b428fa228e8cc8e2fe515b6a904001f53
gkpole <89931423+gkpole@users.noreply.github.com>
2023-01-07 21:23:06 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-TOKEN = 'токен'
-admin_user_id =  #ваш юзер_айди, без скобочек!!!!!
+TOKEN = '5485921311:AAHCRVMZqSyc5_eaSpmDa5B64689GHkAFo'
+admin_user_id = -1001814890080 #ваш юзер_айди, без скобочек!!!!!
